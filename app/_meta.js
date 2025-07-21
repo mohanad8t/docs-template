bcc96f9a5eda6acaa9e519d9e33f8799a85fb9bf
Mohanad Tarek <mohanad8t@gmail.com>
2025-07-22 01:46:54 +0300

    Add files via upload

---
+++ app/_meta.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  introduction: "Introduction",
+  another: "Another Page",
+  advanced: "Advanced (A Folder)",
+    "###": {
+    type: "separator",
+  },
+  about: "About",
+  'contact': {
+  title: 'Contact',
+  href: 'https://mohanad.cc/contact',
+  },
+};
